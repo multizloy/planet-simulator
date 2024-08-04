72af1454ea0f367f4f37b1db013ee1206934c3b3
Renat Shabanov <multizloy@gmail.com>
2024-08-04 12:41:47 +0300

    setup

--- main.py
+++ main.py
@@ -11,3 +11,16 @@ import ctypes
 
 # Enable High Dots Per Inch so the image displayed on the window is sharper.
 ctypes.windll.shcore.SetProcessDpiAwareness(1)
+
+# Configuration
+pygame.init()
+fps = 60
+fps_clock = pygame.time.Clock()
+
+# Window Size
+window_dim = Vector2(800, 800)
+screen = pygame.display.set_mode((int(window_dim.x), int(window_dim.y)))
+
+# all the planets are stored here
+# they will append themselves.
+planets = []
